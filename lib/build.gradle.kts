ad6ce11e51641bc85d444bf746325b8a05a26d4b
zjun <565061763@qq.com>
2024-10-28 12:35:34 +0800

    1.0.0

--- lib/build.gradle.kts
+++ lib/build.gradle.kts
@@ -6,7 +6,7 @@ plugins {
 
 val libGroupId = "com.sd.lib.android"
 val libArtifactId = "keyedstate"
-val libVersion = "1.0.0-beta01"
+val libVersion = "1.0.0"
 
 android {
    namespace = "com.sd.lib.keyedstate"
